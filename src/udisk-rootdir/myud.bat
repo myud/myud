70ee19a49d0f70790613c377fd5b21fdbc46f94f
Ivan <ivan@youzi.com.cn>
2017-10-14 19:08:11 +0800

    发布

--- src/udisk-rootdir/myud.bat
+++ src/udisk-rootdir/myud.bat
@@ -1,41 +1,67 @@
 @echo off
 
-title myud
+title Myud
 color 0f
 
+
+::
 set file1=.ks.cfg.bak
 set file2=.ks.cfg.tmp
+
+set file3=manual-install\.config-network.bak
+set file4=manual-install\.config-network.tmp
 
 call :checkFileExist "%file1%"
 copy /y %file1% ks.cfg >nul
 
+call :checkFileExist "%file3%"
+copy /y %file3% manual-install\config-network >nul
+
+
+::
 echo/
-set /p ipaddr= 请输入你的 IP (默认值 192.168.1.5): 
+set /p ipaddr= 请输入你的 IP (默认:192.168.1.5): 
+
 echo/
-set /p gateway= 请输入你的网关 (默认值 192.168.1.1): 
+set /p gateway= 请输入你的网关 (默认:192.168.1.1): 
+
 echo/
-set /p dns= 请输入你的 DNS (默认值 114.114.114.114): 
+set /p dns= 请输入你的 DNS (默认:114.114.114.114): 
+echo/
 
 if not defined ipaddr (set ipaddr=192.168.1.5)
 if not defined gateway (set gateway=192.168.1.1)
 if not defined dns (set dns=114.114.114.114)
 
-call :replaceStr "ks.cfg" "qinweijunlin" "%ipaddr%" "%file2%"
-call :replaceStr "ks.cfg" "qinweijunlin" "%gateway%" "%file2%"
-call :replaceStr "ks.cfg" "qinweijunlin" "%dns%" "%file2%"
 
+::
+call :replaceStr "ks.cfg" "##custom##192.168.1.5##" "%ipaddr%" "%file2%"
+call :replaceStr "ks.cfg" "##custom##192.168.1.1##" "%gateway%" "%file2%"
+call :replaceStr "ks.cfg" "##custom##114.114.114.114##" "%dns%" "%file2%"
+
+call :replaceStr "manual-install\config-network" "##custom##192.168.1.5##" "%ipaddr%" "%file4%"
+call :replaceStr "manual-install\config-network" "##custom##192.168.1.1##" "%gateway%" "%file4%"
+call :replaceStr "manual-install\config-network" "##custom##114.114.114.114##" "%dns%" "%file4%"
+
+
+::
 cls
 color 0a
+echo +--------------------------------------------------
+echo ^|
+echo ^|            配置成功
+echo ^|
+echo +--------------------------------------------------
 
-echo/
-echo 配置完成
 echo/
 echo 你的 IP:     %ipaddr%
 echo 你的网关:    %gateway%
 echo 你的 DNS:    %dns%
 echo/
 
-pause >nul
+pause>nul
+exit 0
+
 
 :: --------------------------------------------------
 :: Function - 检查文件是否存在
@@ -44,12 +70,13 @@ pause >nul
 :checkFileExist
 
 if not exist %~1 (
-        echo 错误: '%~1' 文件不存在
-        pause >nul
+        echo 错误: '%cd%\%~1' 文件不存在
+        pause>nul
         exit 1
 )
 
 goto:eof
+
 
 :: --------------------------------------------------
 :: Function - 替换字符串
