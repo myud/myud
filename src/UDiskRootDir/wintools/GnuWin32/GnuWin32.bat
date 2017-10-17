df79ec5abb1ef0f3ed2912b35df7409c1b681a15
Ivan <ivan@youzi.com.cn>
2017-10-18 02:58:01 +0800

    发布

--- src/UDiskRootDir/wintools/GnuWin32/GnuWin32.bat
+++ src/UDiskRootDir/wintools/GnuWin32/GnuWin32.bat
@@ -1,8 +1,14 @@
-:: call GnuWin32\GnuWin32.bat
+::+------------------------------------------------------------
+::|     Batch - 检测 GnuWin32 命令
+::+------------------------------------------------------------
+
+:: 使用: call 路径\GnuWin32.bat
+:: 结果: 命令不可以执行就退出
+:: 说明: 
 
 @echo off
 
-set GnuWin32Path=%cd%\GnuWin32
+set GnuWin32Path=%~dp0
 
 set GnuWin32Command01=7za
 set GnuWin32Command02=awk
@@ -13,7 +19,7 @@ set GnuWin32Command06=md5
 set GnuWin32Command07=sed
 set GnuWin32Command08=wget
 
-set path=%GnuWin32Path%\%GnuWin32Command08%\bin;%GnuWin32Path%\%GnuWin32Command07%\bin;%GnuWin32Path%\%GnuWin32Command06%\bin;%GnuWin32Path%\%GnuWin32Command05%\bin;%GnuWin32Path%\%GnuWin32Command04%\bin;%GnuWin32Path%\%GnuWin32Command03%\bin;%GnuWin32Path%\%GnuWin32Command02%\bin;%GnuWin32Path%\%GnuWin32Command01%\bin;%path%
+set path=%GnuWin32Path%%GnuWin32Command08%\bin;%GnuWin32Path%%GnuWin32Command07%\bin;%GnuWin32Path%%GnuWin32Command06%\bin;%GnuWin32Path%%GnuWin32Command05%\bin;%GnuWin32Path%%GnuWin32Command04%\bin;%GnuWin32Path%%GnuWin32Command03%\bin;%GnuWin32Path%%GnuWin32Command02%\bin;%GnuWin32Path%%GnuWin32Command01%\bin;%path%
 
 set GnuWin32CommandList=%GnuWin32Command08%,%GnuWin32Command07%,%GnuWin32Command06%,%GnuWin32Command05%,%GnuWin32Command04%,%GnuWin32Command03%,%GnuWin32Command02%,%GnuWin32Command01%
 
