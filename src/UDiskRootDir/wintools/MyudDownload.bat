b77cb25c1494d4535d458425485bec72f3f4d254
Ivan <ivan@youzi.com.cn>
2017-10-20 16:11:01 +0800

    发布

--- src/UDiskRootDir/wintools/MyudDownload.bat
+++ src/UDiskRootDir/wintools/MyudDownload.bat
@@ -14,60 +14,141 @@ call %MyudDownloadPath%GnuWin32\GnuWin32.bat
 
 :: 变量(下载)
 
-set DownloadHR=/h
 
-set DownloadDirA=%MyudDownloadPath%..
-set DownloadUrlA=http://mirrors.aliyun.com/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1708.iso
-set DownloadMd5A=5848f2fd31c7acf3811ad88eaca6f4aa
-set DownloadFileA=CentOS-7-x86_64-Minimal-1708.iso
 
-set DownloadDirB=%MyudDownloadPath%..\LMT
 
-set DownloadUrlB1=https://gitee.com/mydownload/myud-attachment/raw/master/attachment/CentOS-7-x86_64-NetInstall-1708/CentOS-7-x86_64-NetInstall-1708.zip.001
-set DownloadUrlB2=https://gitee.com/mydownload/myud-attachment/raw/master/attachment/CentOS-7-x86_64-NetInstall-1708/CentOS-7-x86_64-NetInstall-1708.zip.002
-set DownloadUrlB3=https://gitee.com/mydownload/myud-attachment/raw/master/attachment/CentOS-7-x86_64-NetInstall-1708/CentOS-7-x86_64-NetInstall-1708.zip.003
-set DownloadUrlB4=https://gitee.com/mydownload/myud-attachment/raw/master/attachment/CentOS-7-x86_64-NetInstall-1708/CentOS-7-x86_64-NetInstall-1708.zip.004
-set DownloadUrlB5=https://gitee.com/mydownload/myud-attachment/raw/master/attachment/CentOS-7-x86_64-NetInstall-1708/CentOS-7-x86_64-NetInstall-1708.zip.005
 
-set DownloadMd5B1=6e919504f50d4f49b94366e267fd557a
-set DownloadMd5B2=80b73ed0bb84079260181b318cd2b434
-set DownloadMd5B3=a922b8209d677d0909a3fdf6cc75420e
-set DownloadMd5B4=f8e3f8659eddddcbe60366d13dfd0816
-set DownloadMd5B5=b7154e6273687382a6382234d095cb56
 
-:: 变量(解压)
 
-set DownloadFileB=CentOS-7-x86_64-NetInstall-1708.iso
-set DownloadMd5B=75acc54b5825edb96a5a843996fd578b
 
-set DownloadFileB1=CentOS-7-x86_64-NetInstall-1708.zip.001
-set DownloadFileB2=CentOS-7-x86_64-NetInstall-1708.zip.002
-set DownloadFileB3=CentOS-7-x86_64-NetInstall-1708.zip.003
-set DownloadFileB4=CentOS-7-x86_64-NetInstall-1708.zip.004
-set DownloadFileB5=CentOS-7-x86_64-NetInstall-1708.zip.005
 
-set DownloadFileList=%DownloadFileB1%,%DownloadFileB2%,%DownloadFileB3%,%DownloadFileB4%,%DownloadFileB5%
 
-set MyudDownloadNum=0
 
-:: 测试阿里云
 
-set AliyunTestUrl=http://mirrors.aliyun.com/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1708.torrent
-set AliyunTestMd5=f01b8a4a42218b55f0ced67a0875f06e
 
-if not exist %DownloadDirA%\%DownloadFileA% (
-        call MyudCaw.bat "/r" "%userprofile%\desktop" "%AliyunTestMd5%" "%AliyunTestUrl%"
-)
+
+
+
+
+
+
+
+
+
+:: 变量(下载与解压)
+
+set DownloadHoldRemove=/h
+
+set MinIsoDir=%MyudDownloadPath%..
+set MinIsoMd5=5848f2fd31c7acf3811ad88eaca6f4aa
+set MinIsoUrl=http://mirrors.aliyun.com/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1708.iso
+set MinIsoFile=CentOS-7-x86_64-Minimal-1708.iso
+set MinIsoFilePath=%MinIsoDir%\%MinIsoFile%
+
+set NetIsoDir=%MyudDownloadPath%..\LMT
+set NetIsoMd5=75acc54b5825edb96a5a843996fd578b
+rem NetIsoUrl=
+set NetIsoFile=CentOS-7-x86_64-NetInstall-1708.iso
+set NetIsoFilePath=%NetIsoDir%\%NetIsoFile%
+
+rem 目录
+set NetZipDir=%NetIsoDir%
+rem MD5
+set NetZipMd51=6e919504f50d4f49b94366e267fd557a
+set NetZipMd52=80b73ed0bb84079260181b318cd2b434
+set NetZipMd53=a922b8209d677d0909a3fdf6cc75420e
+set NetZipMd54=f8e3f8659eddddcbe60366d13dfd0816
+set NetZipMd55=b7154e6273687382a6382234d095cb56
+rem URL
+set NetZipUrl1=https://gitee.com/mydownload/myud-attachment/raw/master/attachment/CentOS-7-x86_64-NetInstall-1708/CentOS-7-x86_64-NetInstall-1708.zip.001
+set NetZipUrl2=https://gitee.com/mydownload/myud-attachment/raw/master/attachment/CentOS-7-x86_64-NetInstall-1708/CentOS-7-x86_64-NetInstall-1708.zip.002
+set NetZipUrl3=https://gitee.com/mydownload/myud-attachment/raw/master/attachment/CentOS-7-x86_64-NetInstall-1708/CentOS-7-x86_64-NetInstall-1708.zip.003
+set NetZipUrl4=https://gitee.com/mydownload/myud-attachment/raw/master/attachment/CentOS-7-x86_64-NetInstall-1708/CentOS-7-x86_64-NetInstall-1708.zip.004
+set NetZipUrl5=https://gitee.com/mydownload/myud-attachment/raw/master/attachment/CentOS-7-x86_64-NetInstall-1708/CentOS-7-x86_64-NetInstall-1708.zip.005
+rem 文件
+set NetZipFile1=CentOS-7-x86_64-NetInstall-1708.zip.001
+set NetZipFile2=CentOS-7-x86_64-NetInstall-1708.zip.002
+set NetZipFile3=CentOS-7-x86_64-NetInstall-1708.zip.003
+set NetZipFile4=CentOS-7-x86_64-NetInstall-1708.zip.004
+set NetZipFile5=CentOS-7-x86_64-NetInstall-1708.zip.005
+rem 文件路径
+set NetZipFilePath1=%NetZipDir%\%NetZipFile1%
+rem 文件列表
+set NetZipFileList=%NetZipFile1%,%NetZipFile2%,%NetZipFile3%,%NetZipFile4%,%NetZipFile5%
+
+:: 变量(测试阿里云)
+
+set AliyunHoldRemove=/r
+set AliyunDir=%userprofile%\desktop
+set AliyunMd5=f01b8a4a42218b55f0ced67a0875f06e
+set AliyunUrl=http://mirrors.aliyun.com/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1708.torrent
+
+:: 开始测试
 
 :: 开始下载
 
-call MyudCaw.bat "%DownloadHR%" "%DownloadDirA%" "%DownloadMd5A%" "%DownloadUrlA%"
+:: 开始解压
 
-call MyudCaw.bat "%DownloadHR%" "%DownloadDirB%" "%DownloadMd5B1%" "%DownloadUrlB1%"
-call MyudCaw.bat "%DownloadHR%" "%DownloadDirB%" "%DownloadMd5B2%" "%DownloadUrlB2%"
-call MyudCaw.bat "%DownloadHR%" "%DownloadDirB%" "%DownloadMd5B3%" "%DownloadUrlB3%"
-call MyudCaw.bat "%DownloadHR%" "%DownloadDirB%" "%DownloadMd5B4%" "%DownloadUrlB4%"
-call MyudCaw.bat "%DownloadHR%" "%DownloadDirB%" "%DownloadMd5B5%" "%DownloadUrlB5%"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+call MyudCaw.bat "%AliyunHoldRemove%" "%AliyunDir%" "%AliyunMd5%" "%AliyunUrl%"
+
+call MyudCaw.bat "%DownloadHoldRemove%" "%MinIsoDir%" "%MinIsoMd5%" "%MinIsoUrl%"
+
+call MyudCaw.bat "%DownloadHoldRemove%" "%NetZipDir%" "%NetZipMd51%" "%NetZipUrl1%"
+call MyudCaw.bat "%DownloadHoldRemove%" "%NetZipDir%" "%NetZipMd52%" "%NetZipUrl2%"
+call MyudCaw.bat "%DownloadHoldRemove%" "%NetZipDir%" "%NetZipMd53%" "%NetZipUrl3%"
+call MyudCaw.bat "%DownloadHoldRemove%" "%NetZipDir%" "%NetZipMd54%" "%NetZipUrl4%"
+call MyudCaw.bat "%DownloadHoldRemove%" "%NetZipDir%" "%NetZipMd55%" "%NetZipUrl5%"
+
+
+
+
+:: 变量(解压)
+
+
+
+
+
+
+
+
+
+
+
+
 
 :: 开始解压
 
