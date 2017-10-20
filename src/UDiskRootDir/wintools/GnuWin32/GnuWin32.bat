a293688284cae3f3a808f84966dc64bd5b0b56f2
Ivan <ivan@youzi.com.cn>
2017-10-21 01:09:24 +0800

    发布

--- src/UDiskRootDir/wintools/GnuWin32/GnuWin32.bat
+++ src/UDiskRootDir/wintools/GnuWin32/GnuWin32.bat
@@ -1,14 +1,16 @@
 ::+------------------------------------------------------------
-::|     Batch - 检测 GnuWin32 命令
+::      Batch - 检查命令
+::
+::      Usage: GnuWin32
 ::+------------------------------------------------------------
-
-:: 使用: call 绝对路径\GnuWin32.bat
-:: 结果: 命令不可以执行就退出
-:: 说明: 
-
 @echo off
+color 0f
+echo,%~n0 - Running...
 
-set GnuWin32Path=%~dp0
+
+rem start
+set GnuWin32_Name=%~n0
+set GnuWin32_Path=%~dp0
 
 set GnuWin32Command01=7za
 set GnuWin32Command02=awk
@@ -19,18 +21,24 @@ set GnuWin32Command06=md5
 set GnuWin32Command07=sed
 set GnuWin32Command08=wget
 
-set path=%GnuWin32Path%%GnuWin32Command08%\bin;%GnuWin32Path%%GnuWin32Command07%\bin;%GnuWin32Path%%GnuWin32Command06%\bin;%GnuWin32Path%%GnuWin32Command05%\bin;%GnuWin32Path%%GnuWin32Command04%\bin;%GnuWin32Path%%GnuWin32Command03%\bin;%GnuWin32Path%%GnuWin32Command02%\bin;%GnuWin32Path%%GnuWin32Command01%\bin;%path%
+set path=%GnuWin32_Path%%GnuWin32Command08%\bin;%GnuWin32_Path%%GnuWin32Command07%\bin;%GnuWin32_Path%%GnuWin32Command06%\bin;%GnuWin32_Path%%GnuWin32Command05%\bin;%GnuWin32_Path%%GnuWin32Command04%\bin;%GnuWin32_Path%%GnuWin32Command03%\bin;%GnuWin32_Path%%GnuWin32Command02%\bin;%GnuWin32_Path%%GnuWin32Command01%\bin;%path%
 
 set GnuWin32CommandList=%GnuWin32Command08%,%GnuWin32Command07%,%GnuWin32Command06%,%GnuWin32Command05%,%GnuWin32Command04%,%GnuWin32Command03%,%GnuWin32Command02%,%GnuWin32Command01%
 
 for %%a in (%GnuWin32CommandList%) do (
-        %%a --help>nul 2>nul||set GnuWin32Command=%%a
+        %%a --help>nul 2>nul||call :exit "%%a command not found!"
 )
 
-if defined GnuWin32Command (
-        echo,%GnuWin32Command% - Command not found!
-        pause>nul
-        exit 1
-)
+choice /t 2 /d y /n>nul
 
-goto :eof
+
+rem end
+goto:eof
+
+
+rem label
+:exit
+echo,%~n0 - Error: %~1
+pause>nul
+exit 1
+goto:eof
