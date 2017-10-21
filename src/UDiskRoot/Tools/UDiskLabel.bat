132aa5818d281b454e56e59a4aba8d5370c73cfa
Ivan <ivan@youzi.com.cn>
2017-10-21 16:19:49 +0800

    发布

--- src/UDiskRoot/Tools/UDiskLabel.bat
+++ src/UDiskRoot/Tools/UDiskLabel.bat
@@ -1,7 +1,7 @@
 ::+------------------------------------------------------------
-::      Batch - 
+::      Batch - 更改U盘卷标
 ::
-::      Usage: UDiskLabel
+::      Usage: UDiskLabel "卷标"
 ::+------------------------------------------------------------
 @echo off
 color 0f
@@ -12,9 +12,19 @@ rem start
 set UDiskLabel_Name=%~n0
 set UDiskLabel_Path=%~dp0
 
+set UDiskLabelArgument1=%~1
 
+if not defined UDiskLabelArgument1 (
+        call :exit "missing operand!"
+)
 
+for /f "tokens=1-3" %%a in ('wmic logicaldisk get Description^,DeviceID^,VolumeName 2^>nul') do (
+        if /i "%%a"=="可移动磁盘" (
+                label %%b %UDiskLabelArgument1%
+        )
+)
 
+choice /t 1 /d y /n>nul
 
 
 rem end
