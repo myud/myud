1cc3fd15a8ffed691077b6261b0c3082fa4c2bac
Ivan <ivan@youzi.com.cn>
2017-10-21 16:42:21 +0800

    发布

--- src/UDiskRoot/Tools/UDiskLabel.bat
+++ src/UDiskRoot/Tools/UDiskLabel.bat
@@ -12,10 +12,22 @@ rem start
 set UDiskLabel_Name=%~n0
 set UDiskLabel_Path=%~dp0
 
+set UDiskLabelKeyname=HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR\Enum
+
 set UDiskLabelArgument1=%~1
 
 if not defined UDiskLabelArgument1 (
         call :exit "missing operand!"
+)
+
+for /f "tokens=1-3" %%a in ('reg query "%UDiskLabelKeyname%" /v Count 2^>nul') do (
+        if /i "%%a"=="Count" (
+                set /a UDiskLabelCountVar=%%c
+        )
+)
+
+if /i "%UDiskLabelCountVar%"=="0" (
+        call :exit "udisk not found!"
 )
 
 for /f "tokens=1-3" %%a in ('wmic logicaldisk get Description^,DeviceID^,VolumeName 2^>nul') do (
