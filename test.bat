5fad9d6a5900f3b7bc111fd32369c8c27be0b42b
Ivan <ivan@youzi.com.cn>
2017-10-22 06:00:05 +0800

    发布

--- test.bat
+++ test.bat
@@ -1,44 +1,44 @@
 ::+------------------------------------------------------------
-::      Batch - ¼ì²éÃüÁî
+::Batch-¼ì²éÃüÁî
 ::
-::      Usage: GnuWin32
+::Usage:GnuWin32
 ::+------------------------------------------------------------
-@echo off
-color 0f
-echo,%~n0 - Running...
+@echooff
+color0f
+echo,%~n0-Running...
 
 
-rem start
-set GnuWin32_Name=%~n0
-set GnuWin32_Path=%~dp0
+remstart
+setGnuWin32_Name=%~n0
+setGnuWin32_Path=%~dp0
 
-set GnuWin32Command01=7za
-set GnuWin32Command02=awk
-set GnuWin32Command03=dos2unix
-set GnuWin32Command04=grep
-set GnuWin32Command05=iconv
-set GnuWin32Command06=md5
-set GnuWin32Command07=sed
-set GnuWin32Command08=wget
+setGnuWin32Command01=7za
+setGnuWin32Command02=awk
+setGnuWin32Command03=dos2unix
+setGnuWin32Command04=grep
+setGnuWin32Command05=iconv
+setGnuWin32Command06=md5
+setGnuWin32Command07=sed
+setGnuWin32Command08=wget
 
-set path=%GnuWin32_Path%%GnuWin32Command08%\bin;%GnuWin32_Path%%GnuWin32Command07%\bin;%GnuWin32_Path%%GnuWin32Command06%\bin;%GnuWin32_Path%%GnuWin32Command05%\bin;%GnuWin32_Path%%GnuWin32Command04%\bin;%GnuWin32_Path%%GnuWin32Command03%\bin;%GnuWin32_Path%%GnuWin32Command02%\bin;%GnuWin32_Path%%GnuWin32Command01%\bin;%path%
+setpath=%GnuWin32_Path%%GnuWin32Command08%\bin;%GnuWin32_Path%%GnuWin32Command07%\bin;%GnuWin32_Path%%GnuWin32Command06%\bin;%GnuWin32_Path%%GnuWin32Command05%\bin;%GnuWin32_Path%%GnuWin32Command04%\bin;%GnuWin32_Path%%GnuWin32Command03%\bin;%GnuWin32_Path%%GnuWin32Command02%\bin;%GnuWin32_Path%%GnuWin32Command01%\bin;%path%
 
-set GnuWin32CommandList=%GnuWin32Command08%,%GnuWin32Command07%,%GnuWin32Command06%,%GnuWin32Command05%,%GnuWin32Command04%,%GnuWin32Command03%,%GnuWin32Command02%,%GnuWin32Command01%
+setGnuWin32CommandList=%GnuWin32Command08%,%GnuWin32Command07%,%GnuWin32Command06%,%GnuWin32Command05%,%GnuWin32Command04%,%GnuWin32Command03%,%GnuWin32Command02%,%GnuWin32Command01%
 
-for %%a in (%GnuWin32CommandList%) do (
-        %%a --help>nul 2>nul||call :exit "%%a command not found!"
+for%%ain(%GnuWin32CommandList%)do(
+%%a--help>nul2>nul||call:exit"%%acommandnotfound!"
 )
 
-::choice /t 1 /d y /n>nul
+::choice/t1/dy/n>nul
 
 
-rem end
+remend
 goto:eof
 
 
-rem label
+remlabel
 :exit
-echo,%~n0 - Error: %~1
+echo,%~n0-Error:%~1
 pause>nul
-exit 1
+exit1
 goto:eof
