7dc33b3a04f5b89094314751b3a9079440dc0f3f
Ivan <ivan@youzi.com.cn>
2017-10-23 15:35:56 +0800

    发布

--- resource/tools/MyudMake/MyudMake.bat
+++ resource/tools/MyudMake/MyudMake.bat
@@ -8,6 +8,26 @@ set SelfPath=%~dp0
 set SelfPath=%SelfPath:~0,-1%
 
 
+:::: 
+echo,
+set /p Ipaddr=请输入你的 IP  (默认:192.168.1.5): 
+if not defined Ipaddr (
+        set Ipaddr=192.168.1.5
+)
+
+echo,
+set /p Gateway=请输入你的网关 (默认:192.168.1.1): 
+if not defined Gateway (
+        set Gateway=192.168.1.1
+)
+
+echo,
+set /p DNS=请输入你的 DNS (默认:114.114.114.114): 
+if not defined DNS (
+        set DNS=114.114.114.114
+)
+
+
 :::::::: End ::::::::
 
 pause>nul
