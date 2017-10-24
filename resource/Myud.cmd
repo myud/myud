f3fb81972d1b4cb3e3427f2044173801aae4ee1e
Ivan <ivan@youzi.com.cn>
2017-10-24 15:50:37 +0800

    发布

--- resource/Myud.cmd
+++ resource/Myud.cmd
@@ -8,15 +8,68 @@ set SelfPath=%SelfPath:~0,-1%
 
 
 
-:::: ####################
-echo,刘德华
 
 
-:::: End
+
+
+
+
+
+
+
+
+
+
+
+
+:::: 交互
+echo,
+set /p Ipaddr=请输入你的 IP  (默认:192.168.1.5): 
+if not defined Ipaddr (
+        set Ipaddr=192.168.1.5
+)
+
+echo,
+set /p Gateway=请输入你的网关 (默认:192.168.1.1): 
+if not defined Gateway (
+        set Gateway=192.168.1.1
+)
+
+echo,
+set /p DNS=请输入你的 DNS (默认:114.114.114.114): 
+if not defined DNS (
+        set DNS=114.114.114.114
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 pause>nul
-
-
-
 :::::::: Main End :::::::::::
 goto:eof
 :::::::: Label Start ::::::::
+:::: exit "message"
+:exit
+
+echo,%~n0 - Error: %~1
+pause>nul
+exit 1
+
+goto:eof
