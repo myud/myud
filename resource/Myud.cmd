f36c683a20c5cc6cfadbf8d1608f3bb964fee8ce
Ivan <ivan@youzi.com.cn>
2017-10-24 15:26:04 +0800

    发布

---
+++ resource/Myud.cmd
@@ -0,0 +1,22 @@
+:::: Start
+@echo off
+title %~n0
+color 0f
+
+set SelfPath=%~dp0
+set SelfPath=%SelfPath:~0,-1%
+
+
+
+:::: ####################
+echo,ΑυµΒ»ª
+
+
+:::: End
+pause>nul
+
+
+
+:::::::: Main End :::::::::::
+goto:eof
+:::::::: Label Start ::::::::
