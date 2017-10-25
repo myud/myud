bf738d48a3ed6377ef7b4bd6b735ca21a70e69aa
Ivan <ivan@youzi.com.cn>
2017-10-25 18:10:50 +0800

    发布

--- resource/Myud.cmd
+++ resource/Myud.cmd
@@ -1,19 +1,39 @@
-:::: 初始化
-@echo off
+@ECHO OFF
+REM 初始化
 title %~n0
 color 0f
 
-set SelfPath=%~dp0
-set SelfPath=%SelfPath:~0,-1%
+set MyudPath=%~dp0
+set MyudPath=%MyudPath:~0,-1%
 
 choice /t 1 /d y /n>nul
 
-call :checkUDisk "MYUD"
+if not exist %MyudPath%\cmd\MyudFunc.cmd (
+        echo,%~n0 - Main Error: %MyudPath%\cmd\MyudFunc.cmd 文件不存在!
+        pause>nul
+        exit 1
+)
 
-call :checkCommand
 
 
-:::: 交互
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+REM 交互
 echo,
 set /p Ipaddr=请输入你的 IP  (默认:192.168.1.5): 
 if not defined Ipaddr (
@@ -39,7 +59,7 @@ echo,
 pause>nul
 
 
-:::: 
+REM 
 cls
 echo,正在运行, 请稍后...
 
@@ -49,7 +69,8 @@ choice /t 1 /d y /n>nul
 
 
 
-:::: 结束
+
+REM 结束
 cls
 echo,已完成, 请退出...
 
@@ -60,138 +81,25 @@ echo,你的 DNS:        %DNS%
 
 echo,
 pause>nul
-:::::::: Main End :::::::::::
-goto:eof
-:::::::: Label Start ::::::::
-:::: exit "message"
-:exit
-
-echo,%~n0 - Error: %~1
-pause>nul
-exit 1
-
-goto:eof
+@GOTO:EOF
 
 
-:::: checkUDisk "label"
-:checkUDisk
+:
+REM   arg1: 
+REM return: 
+SETLOCAL
 
-set CheckUDiskKeyname=HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR\Enum
+:BEGIN
 
-set CheckUDiskArgument1=%~1
-
-if not defined CheckUDiskArgument1 (
-        rem call :exit "checkUDisk: missing operand!"
-        call :exit "checkUDisk: 缺少参数!"
+:END
+(ENDLOCAL
+        
 )
-
-for /f "tokens=1-3" %%a in ('reg query "%CheckUDiskKeyname%" /v Count 2^>nul') do (
-        if /i "%%a"=="Count" (
-                set /a CheckUDiskCountVar=%%c
-        )
-)
-
-if /i "%CheckUDiskCountVar%"=="0" (
-        rem call :exit "udisk not found!"
-        call :exit "U盘不存在!"
-)
-
-for /f "tokens=1-3" %%a in ('wmic logicaldisk get Description^,DeviceID^,VolumeName 2^>nul') do (
-        if /i "%%a"=="可移动磁盘" (
-                if /i "%%b"=="%SelfPath%" (
-                        set CheckUDiskDeviceID=%%b
-                        set CheckUDiskVolumeName=%%c
-                )
-        )
-)
-
-if not defined CheckUDiskDeviceID (
-        call :exit "请将此程序复制到U盘根目录下运行!"
-)
-
-if /i not "%CheckUDiskVolumeName:~0,4%"=="MYUD" (
-        label %CheckUDiskDeviceID% %CheckUDiskArgument1%
-)
-
-goto:eof
+GOTO:EOF
 
 
-:::: checkCommand
-:checkCommand
-
-set CheckCommand1=7za
-set CheckCommand2=awk
-set CheckCommand3=dos2unix
-set CheckCommand4=grep
-set CheckCommand5=iconv
-set CheckCommand6=md5
-set CheckCommand7=sed
-set CheckCommand8=wget
-
-set path=%SelfPath%\cmd\%CheckCommand8%\bin;%SelfPath%\cmd\%CheckCommand7%\bin;%SelfPath%\cmd\%CheckCommand6%\bin;%SelfPath%\cmd\%CheckCommand5%\bin;%SelfPath%\cmd\%CheckCommand4%\bin;%SelfPath%\cmd\%CheckCommand3%\bin;%SelfPath%\cmd\%CheckCommand2%\bin;%SelfPath%\cmd\%CheckCommand1%\bin;%path%
-
-set CheckCommandList=%CheckCommand8%,%CheckCommand7%,%CheckCommand6%,%CheckCommand5%,%CheckCommand4%,%CheckCommand3%,%CheckCommand2%,%CheckCommand1%
-
-for %%a in (%CheckCommandList%) do (
-        if not exist %SelfPath%\cmd\%%a\bin\%%a.exe (
-                rem call :exit "%%a command not found!"
-                call :exit "%%a 命令不存在!"
-        )
-)
-
-goto:eof
-
-
-:::: replaceStr "oldstr" "newstr" "file"                   # 不会改变原来的文件编码
-:replaceStr
-
-set ReplaceStrArgument1=%~1
-set ReplaceStrArgument2=%~2
-set ReplaceStrArgument3=%~3
-
-if not defined ReplaceStrArgument1 (
-        rem call :exit "replaceStr: missing oldstr!"
-        call :exit "replaceStr: 缺少原字符串!"
-)
-
-if not defined ReplaceStrArgument2 (
-        rem call :exit "replaceStr: missing newstr!"
-        call :exit "replaceStr: 缺少新字符串!"
-)
-
-if not defined ReplaceStrArgument3 (
-        rem call :exit "replaceStr: missing filename!"
-        call :exit "replaceStr: 缺少文件名称!"
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-goto:eof
+REM # Main Var
+REM MyudPath
+REM Ipaddr
+REM Gateway
+REM DNS
---
+++ resource/cmd/MyudFunc.cmd
@@ -0,0 +1,18 @@
+@ECHO OFF
+REM 
+
+@GOTO:EOF
+
+
+:
+REM   arg1: 
+REM return: 
+SETLOCAL
+
+:BEGIN
+
+:END
+(ENDLOCAL
+        
+)
+GOTO:EOF
