41d92df2641ec5a9e656666547862244bd2194ff
Ivan <ivan@youzi.com.cn>
2017-10-25 21:04:50 +0800

    发布

--- resource/cmd/MyudFunc.cmd
+++ resource/cmd/MyudFunc.cmd
@@ -1,15 +1,29 @@
-@ECHO OFF
-REM 
-
-@GOTO:EOF
 
 
-:
-REM   arg1: 
+
+
+
+
+
+
+
+
+
+
+
+:Error
+REM   func: 提示信息并且退出
+REM   arg1: LabelName - 标签名称
+REM   arg2: Message   - 提示信息
 REM return: 
 SETLOCAL
 
+set LabelName=%~1
+set Message=%~2
+
 :BEGIN
+
+
 
 :END
 (ENDLOCAL
