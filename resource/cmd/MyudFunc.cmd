7c0d842d563172ad4969089cb7424c7d8d10701c
Ivan <ivan@youzi.com.cn>
2017-10-26 06:03:40 +0800

    发布

--- resource/cmd/MyudFunc.cmd
+++ resource/cmd/MyudFunc.cmd
@@ -1,4 +1,5 @@
 @ECHO OFF
+
 REM 检查参数
 set FuncPath=%~dp0
 set FuncPath=%FuncPath:~0,-1%
@@ -13,22 +14,44 @@ set Arg7=%~7
 set Arg8=%~8
 set Arg9=%~9
 
-
-
-
-
-
-call :Error
-
+if defined Arg9 (
+        call :%Arg1% "%Arg2%" "%Arg3%" "%Arg4%" "%Arg5%" "%Arg6%" "%Arg7%" "%Arg8%" "%Arg9%" 2>nul||call :Error "Main" "%Arg1% 标签不存在"
+) else (
+        if defined Arg8 (
+                call :%Arg1% "%Arg2%" "%Arg3%" "%Arg4%" "%Arg5%" "%Arg6%" "%Arg7%" "%Arg8%" 2>nul||call :Error "Main" "%Arg1% 标签不存在"
+        ) else (
+                if defined Arg7 (
+                        call :%Arg1% "%Arg2%" "%Arg3%" "%Arg4%" "%Arg5%" "%Arg6%" "%Arg7%" 2>nul||call :Error "Main" "%Arg1% 标签不存在"
+                ) else (
+                        if defined Arg6 (
+                                call :%Arg1% "%Arg2%" "%Arg3%" "%Arg4%" "%Arg5%" "%Arg6%" 2>nul||call :Error "Main" "%Arg1% 标签不存在"
+                        ) else (
+                                if defined Arg5 (
+                                        call :%Arg1% "%Arg2%" "%Arg3%" "%Arg4%" "%Arg5%" 2>nul||call :Error "Main" "%Arg1% 标签不存在"
+                                ) else (
+                                        if defined Arg4 (
+                                                call :%Arg1% "%Arg2%" "%Arg3%" "%Arg4%" 2>nul||call :Error "Main" "%Arg1% 标签不存在"
+                                        ) else (
+                                                if defined Arg3 (
+                                                        call :%Arg1% "%Arg2%" "%Arg3%" 2>nul||call :Error "Main" "%Arg1% 标签不存在"
+                                                ) else (
+                                                        if defined Arg2 (
+                                                                call :%Arg1% "%Arg2%" 2>nul||call :Error "Main" "%Arg1% 标签不存在"
+                                                        ) else (
+                                                                if defined Arg1 (
+                                                                        call :%Arg1% 2>nul||call :Error "Main" "%Arg1% 标签不存在"
+                                                                ) else (
+                                                                        call :Error "Main" "缺少标签名称"
+)))))))))
 
 
 @GOTO:EOF
 
 
 :Error
-REM   func: 提示信息并且退出
-REM   arg1: LabelName - 标签名称
-REM   arg2: Message   - 提示信息
+REM   func: 输出错误信息并且退出
+REM   arg1: LabelName =Main            出现错误的标签名称
+REM   arg2: Message   =未知错误        想要输出的错误信息
 REM return: 
 SETLOCAL
 
@@ -38,11 +61,11 @@ set Message=%~2
 :BEGIN
 
 if not defined LabelName (
-        set LabelName=Unknown
+        set LabelName=Main
 )
 
 if not defined Message (
-        set Message=unknown
+        set Message=未知错误
 )
 
 echo,%~n0 - %LabelName% Error: %Message%!
