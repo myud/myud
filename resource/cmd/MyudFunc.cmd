e5f036db3eae4c17c5d7704681fa1e97fb8f41f9
Ivan <ivan@youzi.com.cn>
2017-10-26 19:51:04 +0800

    发布

--- resource/cmd/MyudFunc.cmd
+++ resource/cmd/MyudFunc.cmd
@@ -1,27 +1,15 @@
 @ECHO OFF
 
-
 set FuncPath=%~dp0
 set FuncPath=%FuncPath:~0,-1%
 
 set FuncName=%~1
 
-
 if defined FuncName (
-        call :%FuncName% %2 %3 %4 %5 %6 %7 %8 %9 2>nul||call :Error "Main" "%FuncName% 标签不存在或语法错误"
+        call :%FuncName% %2 %3 %4 %5 %6 %7 %8 %9 2>nul||call :Error "Main" "%FuncName% 语法错误或者不存在"
 ) else (
-        
         call :Error "Main" "缺少参数 标签名称"
 )
-
-
-
-
-
-
-
-
-
 
 @GOTO:EOF
 
