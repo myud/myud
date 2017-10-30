c3298be0215a9e1a96be775ea87c21cd53e16556
Ivan <ivan@youzi.com.cn>
2017-10-31 01:07:07 +0800

    发布

---
+++ resource/cmd/Myud.cmd
@@ -0,0 +1,152 @@
+@ECHO OFF
+
+REM 初始化
+title %~n0
+color 0f
+
+set MyudPath=%~dp0
+set MyudPath=%MyudPath:~0,-1%
+
+set MyudFunc=%MyudPath%\MyudFunc.cmd
+set MenuNum=3
+
+choice /t 1 /d y /n>nul
+
+if not exist %MyudFunc% (
+        echo,%~n0 - Main Error: %MyudFunc% 文件不存在!
+        pause>nul
+        exit 1
+)
+
+REM Usage: call %MyudFunc% 标签名称 "参数1" "参数2" "参数3"
+
+call %MyudFunc% CheckUDisk "MYUD"
+
+call %MyudFunc% CheckCommand
+
+REM 交互 设置网络与密码
+cls&echo,(1/%MenuNum%) 正在配置 CentOS 系统:
+echo,
+
+echo,
+set /p Ipaddr=请输入你的  IP    (默认:192.168.1.5): 
+if not defined Ipaddr (
+        set Ipaddr=192.168.1.5
+)
+
+echo,
+set /p Gateway=请输入你的网关    (默认:192.168.1.1): 
+if not defined Gateway (
+        set Gateway=192.168.1.1
+)
+
+echo,
+set /p DNS=请输入你的 DNS    (默认:114.114.114.114): 
+if not defined DNS (
+        set DNS=114.114.114.114
+)
+
+echo,
+set /p RootPassword=请输入 root  密码 (默认:123456): 
+if not defined RootPassword (
+        set RootPassword=123456
+)
+
+echo,
+set /p AdminPassword=请输入 admin 密码 (默认:123456): 
+if not defined AdminPassword (
+        set AdminPassword=123456
+)
+
+REM 交互 设置U盘
+cls&echo,(2/%MenuNum%) 启用或关闭 Myud 功能:
+echo,
+
+echo,
+set /p MountUDisk=是否启用自动挂载U盘 [Y/N]: 
+
+if /i "%MountUDisk%"=="Y" (
+        echo,
+        set /p Var=请插入需要挂载的U盘 ^(支持多个U盘^): 
+        call %MyudFunc% UDiskAttr
+)
+
+REM 交互 设置硬盘
+cls&echo,(3/%MenuNum%) 启用或关闭 Myud 功能:
+echo,
+
+echo,
+set /p MountDisk=是否启用挂载新硬盘 [Y/N]: 
+
+if /i "%MountDisk%"=="Y" (
+        call %MyudFunc% InteractiveDisk
+)
+
+REM 交互 结束
+cls&echo,配置完成!
+echo,
+
+if /i "%MountUDisk%"=="Y" (
+        if not exist %FuncPath%\UDiskAttr.tmp (
+                call %MyudFunc% Error "Main" "%FuncPath%\UDiskAttr.tmp 文件不存在"
+        )
+)
+
+if /i "%MountDisk%"=="Y" (
+        if not exist %FuncPath%\InteractiveDisk.tmp (
+                call %MyudFunc% Error "Main" "%FuncPath%\InteractiveDisk.tmp 文件不存在"
+        )
+)
+
+echo,
+echo,你的  IP:          %Ipaddr%
+echo,你的网关:          %Gateway%
+echo,你的 DNS:          %DNS%
+
+echo,
+echo,root  密码:        %RootPassword%
+echo,admin 密码:        %AdminPassword%
+
+if /i "%MountUDisk%"=="Y" (
+        echo,
+        for /f "tokens=1-6" %%a in ('findstr .* %FuncPath%\UDiskAttr.tmp') do (
+                echo,U盘序列号:         %%f
+        )
+)
+
+if /i "%MountDisk%"=="Y" (
+        REM 如果 in 命令的返回值或字符串为空, 空格, 制表符, 都不会执行 for 里面的语句
+        for /f "tokens=1-4" %%a in ('findstr .* %FuncPath%\InteractiveDisk.tmp') do (
+                echo,
+                echo,设备名称:          %%b
+                echo,挂载点:            %%d
+        )
+)
+
+echo,
+echo,
+echo,请按任意键开始安装...
+echo,
+pause>nul
+
+REM 开始安装
+cls&echo,正在安装 请稍后...
+echo,
+
+REM 更改文件
+call %MyudFunc% ChangeFile
+
+REM 下载
+call %MyudFunc% DownloadISO
+
+REM 全部结束
+cls&echo,安装完成!
+echo,
+
+echo,
+echo,请按任意键退出...
+echo,
+pause>nul
+exit 0
+
+@GOTO:EOF
---
+++ resource/cmd/MyudFunc.cmd
@@ -0,0 +1,721 @@
+@ECHO OFF
+
+set FuncPath=%~dp0
+set FuncPath=%FuncPath:~0,-1%
+
+set FuncName=%~1
+
+REM 标签最后一条语句执行失败也会显示 标签不存在
+if defined FuncName (
+        call :%FuncName% %2 %3 %4 %5 %6 %7 %8 %9||(cls&call :Error "Main" "%FuncName% 标签不存在")
+) else (
+        call :Error "Main" "缺少参数 标签名称"
+)
+
+@GOTO:EOF
+
+
+:Error
+REM   func: 输出错误信息并且退出
+REM   arg1: LabelName =Unknown         出现错误的标签名称
+REM   arg2: Message   =未知错误        想要输出的错误信息
+REM return: 
+SETLOCAL
+
+set LabelName=%~1
+set Message=%~2
+
+:BEGIN
+
+if not defined LabelName (
+        set LabelName=Unknown
+)
+
+if not defined Message (
+        set Message=未知错误
+)
+
+echo,%~n0 - %LabelName% Error: %Message%!
+pause>nul
+exit 1
+
+:END
+(ENDLOCAL
+        
+)
+GOTO:EOF
+
+
+:Argument
+REM   func: 检查参数是否存在
+REM   arg1: LabelName =        出现错误的标签名称
+REM   arg2: ArgList   =        需要检查的参数列表, 如: arg1,arg2,arg3
+REM return: 
+SETLOCAL
+
+set LabelName=%~1
+set ArgList=%~2
+
+:BEGIN
+
+if not defined ArgList (
+        call :Error "Argument" "缺少参数 参数列表"
+)
+
+for %%a in (%ArgList%) do (
+        if not defined %%a (
+                call :Error "%LabelName%" "缺少参数 %%a"
+        )
+)
+
+:END
+(ENDLOCAL
+        
+)
+GOTO:EOF
+
+
+:Directory
+REM   func: 将目录的相对路径转换为绝对路径
+REM   arg1: Dir =          目录的相对路径或绝对路径
+REM return: DirPath        目录的绝对路径
+SETLOCAL
+
+set Dir=%~1
+
+set Current=%cd%
+
+:BEGIN
+
+call :Argument "Directory" "Dir"
+
+if /i "%Current:~-1%"=="\" (
+        set Current=%Current:~0,-1%
+)
+
+if /i "%Dir:~0,1%"=="\" (
+        set Dir=%Dir:~1%
+)
+
+echo,%Dir%|findstr /i "^[c-z]:\\\\">nul 2>nul||set Dir=%Current%\%Dir%
+
+if /i "%Dir:~-1%"=="\" (
+        if /i not "%Dir:~-2%"==":\" (
+                set Dir=%Dir:~0,-1%
+        )
+)
+
+:END
+(ENDLOCAL
+        set DirPath=%Dir%
+)
+GOTO:EOF
+
+
+:CheckUDisk
+REM   func: 检查此程序是否位于U盘并更改U盘卷标
+REM   arg1: UDiskLabel =        新的U盘卷标
+REM return: 
+SETLOCAL
+
+set UDiskLabel=%~1
+
+set KeyName=HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR\Enum
+
+:BEGIN
+
+call :Argument "CheckUDisk" "UDiskLabel"
+
+for /f "tokens=1-3" %%a in ('reg query "%KeyName%" /v Count 2^>nul') do (
+        if /i "%%a"=="Count" (
+                set /a CountVar=%%c
+        )
+)
+
+if /i "%CountVar%"=="0" (
+        call :Error "CheckUDisk" "U盘不存在"
+)
+
+for /f "tokens=1-3" %%a in ('wmic logicaldisk get Description^,DeviceID^,VolumeName 2^>nul') do (
+        if /i "%%a"=="可移动磁盘" (
+                if /i "%%b\cmd"=="%FuncPath%" (
+                        set DeviceID=%%b
+                        set VolumeName=%%c
+                )
+        )
+)
+
+if not defined DeviceID (
+        call :Error "CheckUDisk" "请将此程序复制到U盘根目录下运行"
+)
+
+echo,%VolumeName%|findstr /i "^%UDiskLabel%[0-9]*$">nul 2>nul||label %DeviceID% %UDiskLabel%
+
+:END
+(ENDLOCAL
+        
+)
+GOTO:EOF
+
+
+:CheckCommand
+REM   func: 检查命令是否存在并设置环境变量
+REM   arg1: 
+REM return: CommandPath        所有命令的路径
+SETLOCAL
+
+set Command1=7za
+set Command2=awk
+set Command3=dos2unix
+set Command4=grep
+set Command5=iconv
+set Command6=md5
+set Command7=sed
+set Command8=wget
+
+set CommandList=%Command8%,%Command7%,%Command6%,%Command5%,%Command4%,%Command3%,%Command2%,%Command1%
+
+set PathList=%FuncPath%\%Command8%\bin;%FuncPath%\%Command7%\bin;%FuncPath%\%Command6%\bin;%FuncPath%\%Command5%\bin;%FuncPath%\%Command4%\bin;%FuncPath%\%Command3%\bin;%FuncPath%\%Command2%\bin;%FuncPath%\%Command1%\bin
+
+:BEGIN
+
+for %%a in (%CommandList%) do (
+        if not exist %FuncPath%\%%a\bin\%%a.exe (
+                call :Error "CheckCommand" "%%a 命令不存在"
+        )
+)
+
+:END
+(ENDLOCAL
+        REM 将局部变量 PathList 的值传递给全局变量 CommandPath
+        set CommandPath=%PathList%
+)
+
+set Path=%CommandPath%;%Path%
+GOTO:EOF
+
+
+:MyudCaw
+REM   func: Myud 的下载工具, 有检测 MD5 的功能
+REM   arg1: HoldRemove =/r          可选, /r: 删除原文件重新下载, /h: 保留原文件不要下载 (如果存在 MD5 会直接检测 MD5)
+REM   arg2: Dir        =%cd%        可选, 文件的保存目录, 默认值为当前目录
+REM   arg3: MD5        =            可选, 文件的 MD5 值
+REM   arg4: URL        =            必选, 下载地址
+REM return: 
+SETLOCAL
+
+set HoldRemove=/r
+set Dir=%cd%
+
+set Arg1=%~1
+set Arg2=%~2
+set Arg3=%~3
+set Arg4=%~4
+
+:BEGIN_MyudCaw
+
+::::call :CheckCommand
+
+REM 变量 HoldRemove, Dir, MD5, URL, File, Http, FilePath
+if defined Arg4 (
+        set HoldRemove=%Arg1%
+        set Dir=%Arg2%
+        set MD5=%Arg3%
+        set URL=%Arg4%
+) else (
+        if defined Arg3 (
+                set HoldRemove=%Arg1%
+                set Dir=%Arg2%
+                set URL=%Arg3%
+        ) else (
+                if defined Arg2 (
+                        set Dir=%Arg1%
+                        set URL=%Arg2%
+                ) else (
+                        if defined Arg1 (
+                                set URL=%Arg1%
+                        ) else (
+                                call :Error "MyudCaw" "缺少参数 URL"
+))))
+
+for /f %%a in ('echo,%URL%^|awk -F/ "{ if (NF>3) { print $NF } }"') do (
+        set File=%%a
+)
+
+for /f "tokens=1* delims=:" %%a in ("%URL%") do (
+        set Http=%%a
+)
+
+REM 检测 URL
+echo,%URL%|grep "^http[s]*://.">nul 2>nul||call :Error "MyudCaw" "缺少参数 URL"
+
+if not defined File (
+        call :Error "MyudCaw" "URL 格式错误"
+)
+
+REM 检测目录
+call :Directory %Dir%
+set Dir=%DirPath%
+
+REM 检测 HoldRemove
+if /i not "%HoldRemove%"=="/r" (
+        if /i not "%HoldRemove%"=="/h" (
+                call :Error "MyudCaw" "%HoldRemove% 是无效选项"
+        )
+)
+
+REM 检测 MD5
+if defined MD5 (
+        echo,%MD5%|grep "^[A-Za-z0-9]\{32\}$">nul 2>nul||call :Error "MyudCaw" "MD5 格式错误"
+)
+
+REM 创建目录
+if not exist %Dir% (
+        mkdir %Dir%
+)
+
+set FilePath=%Dir%\%File%
+
+REM 删除文件
+if exist %FilePath% (
+        if /i "%HoldRemove%"=="/r" (
+                del /f %FilePath%
+        )
+)
+
+REM 下载文件并检测 MD5 值
+for /l %%a in (1,1,10) do (
+        
+        if exist %FilePath% (
+                
+                if defined MD5 (
+                        
+                        echo,%~n0 - MyudCaw MD5Check: %FilePath%
+                        
+                        for /f "tokens=1*" %%z in ('md5 %FilePath%') do (
+                                if /i not "%%z"=="%MD5%" (
+                                        del /f %FilePath%
+                                ) else (
+                                        goto END_MyudCaw
+                                )
+                        )
+                        
+                ) else (
+                        goto END_MyudCaw
+                )
+                
+        ) else (
+                
+                if /i "%Http%"=="https" (
+                        wget -c -P %Dir% --no-check-certificate %URL%
+                ) else (
+                        wget -c -P %Dir% %URL%
+                )
+                
+        )
+        
+)
+
+call :Error "MyudCaw" "%File% 下载失败"
+
+:END_MyudCaw
+(ENDLOCAL
+        
+)
+GOTO:EOF
+
+
+:DownloadISO
+REM   func: 下载 ISO 文件
+REM   arg1: 
+REM return: 
+SETLOCAL
+
+REM HoldRemove
+set HR=/h
+
+REM 目录
+set RootDir=%FuncPath%\..
+set LMTDir=%RootDir%\LMT
+set ImagesDir=%LMTDir%\images
+
+REM MD5
+set MinISOMD5=5848f2fd31c7acf3811ad88eaca6f4aa
+set NetISOMD5=75acc54b5825edb96a5a843996fd578b
+
+set NetZipMD51=6e919504f50d4f49b94366e267fd557a
+set NetZipMD52=80b73ed0bb84079260181b318cd2b434
+set NetZipMD53=a922b8209d677d0909a3fdf6cc75420e
+set NetZipMD54=f8e3f8659eddddcbe60366d13dfd0816
+set NetZipMD55=b7154e6273687382a6382234d095cb56
+
+set ImgZipMD5=f01abc3decf5c91ab3617601e091f188
+
+REM URL
+set MinISOURL=http://mirrors.aliyun.com/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1708.iso
+
+set NetZipURL1=https://gitee.com/mydownload/myud-attachment/raw/master/resource/LMT/CentOS-7-x86_64-NetInstall-1708.zip.001
+set NetZipURL2=https://gitee.com/mydownload/myud-attachment/raw/master/resource/LMT/CentOS-7-x86_64-NetInstall-1708.zip.002
+set NetZipURL3=https://gitee.com/mydownload/myud-attachment/raw/master/resource/LMT/CentOS-7-x86_64-NetInstall-1708.zip.003
+set NetZipURL4=https://gitee.com/mydownload/myud-attachment/raw/master/resource/LMT/CentOS-7-x86_64-NetInstall-1708.zip.004
+set NetZipURL5=https://gitee.com/mydownload/myud-attachment/raw/master/resource/LMT/CentOS-7-x86_64-NetInstall-1708.zip.005
+
+set ImgZipURL=https://gitee.com/mydownload/myud-attachment/raw/master/resource/LMT/images.zip
+
+REM 文件
+set MinISOFilePath=%RootDir%\CentOS-7-x86_64-Minimal-1708.iso
+set NetISOFilePath=%LMTDir%\CentOS-7-x86_64-NetInstall-1708.iso
+
+set NetZipFile1=CentOS-7-x86_64-NetInstall-1708.zip.001
+set NetZipFile2=CentOS-7-x86_64-NetInstall-1708.zip.002
+set NetZipFile3=CentOS-7-x86_64-NetInstall-1708.zip.003
+set NetZipFile4=CentOS-7-x86_64-NetInstall-1708.zip.004
+set NetZipFile5=CentOS-7-x86_64-NetInstall-1708.zip.005
+set NetZipFilePath1=%LMTDir%\%NetZipFile1%
+set NetZipFileList=%NetZipFile1%,%NetZipFile2%,%NetZipFile3%,%NetZipFile4%,%NetZipFile5%
+
+set ImgZipFilePath=%LMTDir%\images.zip
+set ImagesFileList=TRANS.TBL,efiboot.img,pxeboot\TRANS.TBL,pxeboot\initrd.img,pxeboot\vmlinuz
+
+REM 阿里云
+set AliyunDir=%UserProfile%\Desktop
+set AliyunMD5=f01b8a4a42218b55f0ced67a0875f06e
+set AliyunURL=http://mirrors.aliyun.com/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1708.torrent
+set AliyunFilePath=%AliyunDir%\CentOS-7-x86_64-Minimal-1708.torrent
+
+:BEGIN_DownloadISO
+
+::::call :CheckCommand
+
+REM 检测阿里云是否已经更新
+if not exist %MinISOFilePath% (
+        call :MyudCaw /r %AliyunDir% %AliyunMD5% %AliyunURL%
+        del /f %AliyunFilePath%
+)
+
+REM 下载 images.zip
+for %%a in (%ImagesFileList%) do (
+        if not exist %ImagesDir%\%%a (
+                set MissingFile=%%a
+        )
+)
+
+if defined MissingFile (
+        call :MyudCaw %HR% %LMTDir% %ImgZipMD5% %ImgZipURL%
+        7za x "%ImgZipFilePath%" -y -aoa -o"%LMTDir%"
+)
+
+REM 下载 CentOS-7-x86_64-NetInstall-1708.iso
+if exist %NetISOFilePath% (
+        echo,%~n0 - DownloadISO MD5Check: %NetISOFilePath%
+        
+        for /f "tokens=1*" %%z in ('md5 %NetISOFilePath%') do (
+                if /i "%%z"=="%NetISOMD5%" (
+                        goto SKIP_DownloadISO
+                )
+        )
+)
+
+call :MyudCaw %HR% %LMTDir% %NetZipMD51% %NetZipURL1%
+call :MyudCaw %HR% %LMTDir% %NetZipMD52% %NetZipURL2%
+call :MyudCaw %HR% %LMTDir% %NetZipMD53% %NetZipURL3%
+call :MyudCaw %HR% %LMTDir% %NetZipMD54% %NetZipURL4%
+call :MyudCaw %HR% %LMTDir% %NetZipMD55% %NetZipURL5%
+
+for /l %%a in (1,1,10) do (
+        
+        for %%b in (%NetZipFileList%) do (
+                if not exist %LMTDir%\%%b (
+                        call :Error "DownloadISO" "%LMTDir%\%%b 文件不存在"
+                )
+        )
+        
+        7za x "%NetZipFilePath1%" -y -aoa -o"%LMTDir%"
+        
+        echo,%~n0 - DownloadISO MD5Check: %NetISOFilePath%
+        
+        for /f "tokens=1*" %%z in ('md5 %NetISOFilePath%') do (
+                if /i "%%z"=="%NetISOMD5%" (
+                        goto SKIP_DownloadISO
+                )
+        )
+        
+)
+
+call :Error "DownloadISO" "%NetZipFilePath1% 解压失败"
+
+:SKIP_DownloadISO
+
+REM 下载 CentOS-7-x86_64-Minimal-1708.iso
+call :MyudCaw %HR% %RootDir% %MinISOMD5% %MinISOURL%
+
+REM 删除压缩包
+if exist %ImgZipFilePath% (
+        del /f %ImgZipFilePath%
+)
+
+for %%a in (%NetZipFileList%) do (
+        if exist %LMTDir%\%%a (
+                del /f %LMTDir%\%%a
+        )
+)
+
+:END_DownloadISO
+(ENDLOCAL
+        
+)
+GOTO:EOF
+
+
+:ReplaceStr
+REM   func: 替换字符串且不会改变原来的文件编码
+REM   arg1: OldStr =        原字符串
+REM   arg1: NewStr =        新字符串
+REM   arg1: File   =        文件的绝对路径或相对路径
+REM return: 
+SETLOCAL
+
+set OldStr=%~1
+set NewStr=%~2
+set File=%~3
+
+:BEGIN
+
+::::call :CheckCommand
+
+call :Argument "ReplaceStr" "OldStr,NewStr,File"
+
+if /i "%File:~-1%"=="\" (
+        call :Error "ReplaceStr" "文件名称格式错误"
+)
+
+call :Directory %File%
+set File=%DirPath%
+
+if not exist %File% (
+        call :Error "ReplaceStr" "%File% 文件不存在"
+)
+
+sed -i "s/%OldStr%/%NewStr%/g" %File%
+
+dos2unix %File%>nul 2>nul
+
+REM 当前目录下
+for %%a in (sed*) do (
+        echo,%%a|grep "^sed[A-Za-z0-9]\{6\}$">nul 2>nul&&del /f %%a
+)
+
+:END
+(ENDLOCAL
+        
+)
+GOTO:EOF
+
+
+:ChangeFile
+REM   func: 更改文件 ks.cfg 与 config_network.sh
+REM   arg1: 
+REM return: 
+SETLOCAL
+
+set KSDefault=%FuncPath%\..\data\ks.cfg.default
+set KS=%FuncPath%\..\ks.cfg
+
+set NetworkDefault=%FuncPath%\..\data\config_network.sh.default
+set Network=%FuncPath%\..\config_network.sh
+
+:BEGIN
+
+REM 先给文件添加内容, 再修改文件, 可以少写转换文件格式与删除临时文件的代码
+REM 添加或修改都要注意元字符
+
+::::call :CheckCommand
+
+REM 检查文件是否存在
+if not exist %KSDefault% (
+        call :Error "ChangeFile" "%KSDefault% 文件不存在"
+)
+
+if not exist %NetworkDefault% (
+        call :Error "ChangeFile" "%NetworkDefault% 文件不存在"
+)
+
+if /i "%MountUDisk%"=="Y" (
+        if not exist %FuncPath%\UDiskAttr.tmp (
+                call :Error "ChangeFile" "%FuncPath%\UDiskAttr.tmp 文件不存在"
+        )
+)
+
+if /i "%MountDisk%"=="Y" (
+        if not exist %FuncPath%\InteractiveDisk.tmp (
+                call :Error "ChangeFile" "%FuncPath%\InteractiveDisk.tmp 文件不存在"
+        )
+)
+
+REM 复制文件
+copy /y %KSDefault% %KS%>nul
+copy /y %NetworkDefault% %Network%>nul
+
+REM 添加硬盘信息
+if /i "%MountDisk%"=="Y" (
+        for /f "tokens=1-4" %%a in ('findstr .* %FuncPath%\InteractiveDisk.tmp') do (
+                sed -i "/##CUSTOM##ADD##/a\mount_device \"%%d\" \"%%b\"" %KS%
+        )
+        
+        del /f %FuncPath%\InteractiveDisk.tmp
+)
+
+REM 添加U盘信息
+if /i "%MountUDisk%"=="Y" (
+        for /f "tokens=1-6" %%a in ('findstr .* %FuncPath%\UDiskAttr.tmp') do (
+                sed -i "/##CUSTOM##ADD##/a\myud_rules \"%%b\" \"%%d\" \"%%f\"" %KS%
+        )
+        
+        del /f %FuncPath%\UDiskAttr.tmp
+)
+
+REM 添加密码
+sed -i "/##CUSTOM##ADD##/a\echo '%AdminPassword%' | passwd --stdin admin" %KS%
+sed -i "/##CUSTOM##ADD##/a\echo '%RootPassword%' | passwd --stdin root" %KS%
+
+REM 修改网络
+call :ReplaceStr "##CUSTOM##IP##" "%Ipaddr%" "%KS%"
+call :ReplaceStr "##CUSTOM##GW##" "%Gateway%" "%KS%"
+call :ReplaceStr "##CUSTOM##DNS##" "%DNS%" "%KS%"
+
+REM 修改 config_network.sh 文件
+call :ReplaceStr "##CUSTOM##IP##" "%Ipaddr%" "%Network%"
+call :ReplaceStr "##CUSTOM##GW##" "%Gateway%" "%Network%"
+call :ReplaceStr "##CUSTOM##DNS##" "%DNS%" "%Network%"
+
+:END
+(ENDLOCAL
+        
+)
+GOTO:EOF
+
+
+:UDiskAttr
+REM   func: 获取所有插入的U盘属性值, 并将其保存到 %FuncPath%\UDiskAttr.tmp 文件中
+REM   arg1: 
+REM return: 
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+set KeyName=HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR\Enum
+
+:BEGIN
+
+for /f "tokens=1-3" %%a in ('reg query "%KeyName%" /v Count 2^>nul') do (
+        if /i "%%a"=="Count" (
+                set /a CountVar=%%c
+        )
+)
+
+if /i "%CountVar%"=="0" (
+        call :Error "UDiskAttr" "U盘不存在"
+)
+
+type nul>%FuncPath%\UDiskAttr.tmp
+
+for /l %%a in (1,1,%CountVar%) do (
+        set /a UDiskNum=%%a-1
+        
+        for /f "tokens=1-3" %%b in ('reg query "%KeyName%" 2^>nul') do (
+                if /i "%%b"=="!UDiskNum!" (
+                        set Attr=%%d
+                        set VID=!Attr:~8,4!
+                        set PID=!Attr:~17,4!
+                        set SN=!Attr:~22!
+                        
+                        echo,VID: !VID! PID: !PID! SN: !SN!>>%FuncPath%\UDiskAttr.tmp
+                )
+        )
+)
+
+:END
+(ENDLOCAL
+        
+)
+GOTO:EOF
+
+
+:InteractiveDisk
+REM   func: 获取用户的输入信息, 并将其保存到 %FuncPath%\InteractiveDisk.tmp 文件中
+REM   arg1: 
+REM return: 
+SETLOCAL
+
+set Var1=0
+
+type nul>%FuncPath%\InteractiveDisk.tmp
+
+:BEGIN_InteractiveDisk
+
+set Var2=
+set Device=
+set Dir=
+
+set /a Var1+=1
+if /i "%Var1:~1%"=="" (
+        set Var2=0
+)
+
+echo,
+set /p Device=请输入设备名称  %Var2%%Var1% (例如:/dev/sdb1): 
+if /i "%Device%"=="Q" (
+        goto END_InteractiveDisk
+)
+
+echo,
+set /p Dir=请输入挂载点    %Var2%%Var1% (例如:/mnt/dir1): 
+if /i "%Dir%"=="Q" (
+        goto END_InteractiveDisk
+)
+
+echo,
+echo,^>^>^>^>^> 如果需要退出当前模式, 进入下一步, 请输入 Q ...
+
+echo,%Device%|findstr "^/dev/[abcdefghijklmnopqrstuvwxyz][abcdefghijklmnopqrstuvwxyz0-9/_]*$">nul 2>nul||set Device=
+echo,%Dir%|findstr /i "^/[a-z0-9_][a-z0-9/_-]*$">nul 2>nul||set Dir=
+
+if defined Device (
+        if defined Dir (
+                echo,Device: %Device% Dir: %Dir%>>%FuncPath%\InteractiveDisk.tmp
+        ) else (
+                echo,
+                echo,%~n0 - InteractiveDisk Error: 挂载点输入错误, 请重新输入!
+        )
+) else (
+        echo,
+        echo,%~n0 - InteractiveDisk Error: 设备名称输入错误, 请重新输入!
+)
+
+goto BEGIN_InteractiveDisk
+
+:END_InteractiveDisk
+(ENDLOCAL
+        
+)
+GOTO:EOF
+
+
+GOTO:EOF ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+:#01
+REM   func: #05
+REM   arg1: #06 =#07        #08
+REM return: #09_over
+SETLOCAL
+
+rem #02
+
+:BEGIN
+
+rem #03
+
+:END
+(ENDLOCAL
+        rem #04
+)
+GOTO:EOF
