bc0599c0bba9ac7ead8590e2af2945d630fa20ae
Ivan <ivan@youzi.com.cn>
2017-11-03 05:30:28 +0800

    发布

---
+++ resource/shell/once/my.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 1. 请将相关的文件存放在该目录下
+# 2. 该目录为当前目录
+# 3. bin 目录下的所有命令均可使用
+# 4. 全局变量:
+#        MYUD_BIN        bin 目录的绝对路径                如: cd ${MYUD_BIN}
+#        MYUD_FOREVER    forever 目录的绝对路径            如: cd ${MYUD_FOREVER}
+#        MYUD_ONCE       once 目录(该目录)的绝对路径       如: cd ${MYUD_ONCE}
+#        MYUD_LOG        log 文件的绝对路径                如: echo "OK" > ${MYUD_LOG}                   # 将 OK 输出到 log 文件中
+#        MYUD_EXEC       my.sh 脚本执行次数的记录          如: [[ "$MYUD_EXEC" == "1" ]] && echo "OK"    # 在脚本第一次运行时输出 OK
+
+
+
+
+
+
+
+
